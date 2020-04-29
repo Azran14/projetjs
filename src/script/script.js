98cddd975422d6bda0971b77b2148d464cfce0e5
Azran14 <arnaud.dumarche@hetic.net>
2020-04-29 14:34:51 +0200

    js

--- src/script/script.js
+++ src/script/script.js
@@ -1,2 +1,13 @@
-const text = document.querySelector(".hw");
-text.innerHTML += "Hello World";
+const hw = document.querySelector(".hw");
+hw.innerHTML += "Hello World";
+
+// OU //
+
+function helloworld(text) {
+  const element = document.createElement("div");
+  const title = document.createTextNode(text);
+  element.appendChild(title);
+  hw.appendChild(element);
+}
+
+helloworld("Hello Wolrd2");
