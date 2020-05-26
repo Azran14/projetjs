9f46e133f8ba803a024128ad8e5da31f09a15a8f
Azran14 <arnaud.dumarche@hetic.net>
2020-05-26 15:29:50 +0200

    add some js

--- src/script/script.js
+++ src/script/script.js
@@ -1,3 +1,6 @@
+import Cookies from "js-cookie";
+import Granim from "granim";
+
 const hw = document.querySelector(".hw");
 hw.innerHTML += "Hello World";
 
@@ -20,44 +23,164 @@ document.addEventListener("DOMContentLoaded", function () {
       this.pages = [
         {
           title: "Homepage",
-          url: "/",
+          url: "#",
           background: "red",
+          content: `
+						<div>
+							Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas nostrum quidem sequi aperiam numquam minima possimus? Atque possimus molestiae libero, voluptas quidem architecto facilis vero delectus nihil provident tempora velit.
+							<ul>
+								<li>lorem ipsum</li>
+								<li>dolor sit</li>
+								<li>amet consectetur</li>
+							</ul>
+						</div>
+					`,
+          dynamisme: () => {
+            const li = document.querySelectorAll("li");
+
+            function toUpper(element) {
+              element.innerText = element.innerText.toUpperCase();
+            }
+
+            li.forEach((element) => {
+              toUpper(element);
+            });
+          },
         },
         {
           title: "Contact",
-          url: "/contact",
+          url: "#contact",
           background: "green",
+          form: true,
+        },
+        {
+          title: "Test",
+          url: "#test",
+          background: "purple",
+          content: `
+						<div>
+							Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas nostrum quidem sequi aperiam numquam minima possimus? Atque possimus molestiae libero, voluptas quidem architecto facilis vero delectus nihil provident tempora velit.
+							<ul>
+								<li>lorem ipsum</li>
+								<li>dolor sit</li>
+								<li>amet consectetur</li>
+							</ul>
+						</div>
+						<div class="parallax-container">
+							<div class="parallax">
+
+							</div>
+						</div>
+						<div>
+							Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas nostrum quidem sequi aperiam numquam minima possimus? Atque possimus molestiae libero, voluptas quidem architecto facilis vero delectus nihil provident tempora velit.
+							<ul>
+								<li>lorem ipsum</li>
+								<li>dolor sit</li>
+								<li>amet consectetur</li>
+							</ul>
+						</div>
+					`,
+          dynamisme: () => {
+            window.addEventListener("scroll", (e) => {
+              let scrollTop = e.target.scrollingElement.scrollTop;
+
+              console.log(scrollTop);
+
+              document.querySelector(
+                ".parallax"
+              ).style.backgroundPosition = `center calc(50% - ${
+                scrollTop / 2
+              }px)`;
+            });
+          },
         },
       ];
     }
 
     getPageByUrl(url) {
-      return this.pages.find((page) => page.url === url);
+      return this.pages.find((page) => page.url == url);
     }
   }
 
   class View {
-    constructor(container) {
-      this.container = document.querySelector(`.hw`);
+    constructor(pages) {
+      this.container = document.querySelector(".hw");
+      this.container.innerHTML = "";
+      this.addHeader(pages);
     }
 
-    changeTitle(word) {
-      this.container.innerHTML = word;
+    run(dynamisme) {
+      dynamisme();
+    }
+
+    addContent(content) {
+      const contentContainer = document.createElement("div");
+      contentContainer.classList.add("content");
+      contentContainer.innerHTML = content;
+
+      this.container.appendChild(contentContainer);
+    }
+
+    addHeader(pages) {
+      pages.forEach((page) => {
+        const button = document.createElement("button");
+
+        button.innerText = page.title;
+
+        this.container.appendChild(button);
+
+        button.addEventListener("click", () => {
+          location.hash = page.url;
+        });
+      });
+    }
+
+    changeTitle(text) {
+      const title = document.createElement("h1");
+
+      title.innerText = text;
+
+      this.container.appendChild(title);
     }
 
     changeBackground(color) {
       this.container.style.background = color;
     }
+
+    addForm() {
+      let input = document.createElement("input");
+      input.setAttribute("type", "text");
+      input.classList.add("input-text");
+
+      this.container.appendChild(input);
+    }
   }
 
   function controller() {
-    const data = new Model();
-    const currentPage = data.getPageByUrl("/");
+    let data = new Model();
 
-    const page = new View("container");
+    const currentPage = data.getPageByUrl(location.hash || "#");
+
+    let page = new View(data.pages);
     page.changeTitle(currentPage.title);
     page.changeBackground(currentPage.background);
+
+    if (currentPage.content) {
+      page.addContent(currentPage.content);
+    }
+
+    if (typeof currentPage.dynamisme === "function") {
+      page.run(currentPage.dynamisme);
+    }
+
+    if (currentPage.form) {
+      page.addForm();
+    }
   }
+
+  window.addEventListener("hashchange", () => {
+    controller();
+  });
 
   controller();
 });
